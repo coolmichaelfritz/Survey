25bcfa9c640f9c207f82174012a63699be8158b7
Jose Rodriguez and Michael Fritz <jose@>
2018-02-12 20:52:19 -0800

    add more options for favorite color and date of birth

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,14 @@ $(document).ready(function()  {
     var userAnimalInput = $("input#userAnimal").val();
     var userFoodInput = $("input#userFood").val();
     var userLocationInput = $("input#userLocation").val();
-
+    var userColor = $("#color").val();
 
     $(".userName").text(userNameInput);
     $(".userFood").text(userFoodInput);
     $(".userAnimal").text(userAnimalInput);
     $(".userLocation").text(userLocationInput);
+    $(".userColor").text(userColor);
+
 
     $("#userInfo").show();
 
